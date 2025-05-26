d69cc3d6d290435784f1a02cd361e08245cc31cb
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-05-26 23:59:07 +0530

    Completed Category Model

---
+++ src/main/java/lnct/project/ECommerce/models/Category.java
@@ -0,0 +1,29 @@
+package lnct.project.ECommerce.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+
+    @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> childCategories;
+}
