4a18bf9f49d48bdc226fa7e92c2ea0dcfceced29
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-05-30 00:39:58 +0530

    registration endpoint added.

---
+++ src/main/java/lnct/project/ECommerce/dtos/RegistrationRequestDto.java
@@ -0,0 +1,25 @@
+package lnct.project.ECommerce.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegistrationRequestDto {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+}
