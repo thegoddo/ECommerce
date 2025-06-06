24f0b2d7bab5ea5269462fa8712fec518cf1712d
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-06-07 01:57:08 +0530

    implement Cart Repository

--- src/main/java/lnct/project/ECommerce/repositories/CartRepo.java
+++ src/main/java/lnct/project/ECommerce/repositories/CartRepo.java
@@ -1,5 +1,7 @@
 package lnct.project.ECommerce.repositories;
 
+import lnct.project.ECommerce.models.Cart;
+import lnct.project.ECommerce.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepo extends JpaRepository<Cart, Integer> {
