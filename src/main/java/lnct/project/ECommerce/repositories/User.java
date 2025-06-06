28767e41fb3b3723e034c9cbef1e592f7c2e64d7
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-06-07 02:49:01 +0530

    Deleted temp files

--- src/main/java/lnct/project/ECommerce/repositories/User.java
+++
@@ -1,7 +0,0 @@
-package lnct.project.ECommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface User extends JpaRepository<User, Integer> {
-    public User findByEmail(String e);
-}
