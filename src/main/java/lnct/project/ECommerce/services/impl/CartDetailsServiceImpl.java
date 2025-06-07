e5dfa568645332efa39920cfb768c95a91302b9e
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-06-07 14:15:55 +0530

    implemented CartDetailsServiceImpl

---
+++ src/main/java/lnct/project/ECommerce/services/impl/CartDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package lnct.project.ECommerce.services.impl;
+
+import lnct.project.ECommerce.payload.CartDetailDto;
+import lnct.project.ECommerce.payload.CartHelp;
+import lnct.project.ECommerce.repositories.CartRepo;
+import lnct.project.ECommerce.repositories.UserRepo;
+import lnct.project.ECommerce.services.CartDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CartDetailsServiceImpl implements CartDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private CartRepo cartRepo;
+
+    @Override
+    public CartDetailDto addProduct(CartHelp cartHelp) {
+        int productId = cartHelp.getProductId();
+        int quantity = cartHelp.getQuantity();
+        String userEmail = cartHelp.getUserEmail();
+
+        return null;
+    }
+}
